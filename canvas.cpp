e7febd498c28d2f2b99f121c783eb2c88e28420f
emerian320 <emile.merian@ulb.be>
2022-01-02 16:02:14 +0100

    constantes

--- canvas.cpp
+++ canvas.cpp
@@ -1,14 +1,12 @@
 #include "canvas.h"
+#include "const.h"
 
-using namespace std;
-
-const int gridSize = 9;
 
 Canvas::Canvas() {
   for (int x = 0; x<gridSize; x++) {
     cells.push_back({});
     for (int y = 0; y<gridSize; y++)
-      cells[x].push_back({{55*x+28, 55*y+28}, 45, 45});
+      cells[x].push_back({{56*x+25, 56*y+25}, 45, 45});
   }
   set_neighbours();
 }
@@ -19,8 +17,8 @@ void Canvas::set_neighbours() {
       cells[x][y].setNeighbours(find_neighbours(x, y));
 }
 
-vector <Cell *>Canvas::find_neighbours(int x, int y) {
-  vector <Cell *> neighbours;
+std::vector <Cell *>Canvas::find_neighbours(int x, int y) {
+  std::vector <Cell *> neighbours;
   for (int a = x-1; a < x+2; a++) {
     for (int b = y-1; b < y+2; b++) {
       if (a >= 0 and b >=0 and a < gridSize and b < gridSize and (a!=x or b!=y)) {
---
+++ const.h
@@ -0,0 +1,18 @@
+/*--------------------------------------------------
+Constants header file
+--------------------------------------------------*/
+
+#ifndef __CONST_H
+#define __CONST_H
+
+const int DIRECTIONS[4][2] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+//canvas.cpp
+const int gridSize = 9;
+
+//main.cpp
+const int windowWidth = 500;
+const int windowHeight = 500;
+const double refreshPerSecond = 60;
+
+#endif
